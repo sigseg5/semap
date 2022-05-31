d114201c21b293e6bc0d9f62eebaf1e2ca7ade68
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-06-01 00:29:55 +0300

    First impl

--- src/dconf.rs
+++ src/dconf.rs
@@ -1,4 +1,7 @@
+// Inspired by https://github.com/kylecorry31/dconf_rs
+
 use std::process::Command;
+use std::process::Output;
 
 pub fn set(key: &str, value: &str) -> Result<(), String> {
     let mut cmd = Command::new("dconf");
@@ -7,4 +10,21 @@ pub fn set(key: &str, value: &str) -> Result<(), String> {
         Ok(_) => Ok(()),
         Err(_) => Err("Unable to set key".to_string()),
     }
+}
+
+pub fn get(key: &str) -> Result<String, String> {
+    let mut cmd = Command::new("dconf");
+    cmd.args(&["read", key]);
+    match cmd.output() {
+        Ok(output) => Ok(get_stdout(output)),
+        Err(_) => Err("Unable to get key".to_string()),
+    }
+}
+
+fn get_stdout(output: Output) -> String {
+    let vs = output.stdout;
+    String::from_utf8(vs)
+        .unwrap()
+        .replace("\'", "")
+        .replace("\n", "")
 }
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,32 @@ use std::time::Duration;
 
 mod dconf;
 
-// TODO: Parse kb_fingerprint from argv
-// TODO: Read remap scheme from argv
+// TODO: Parse kb_fingerprint from argv (may be compile time?)
+// TODO: Read remap scheme from argv?
+// TODO: Add systemd intergration?
 
 fn main() {
     let xkb_opt = "/org/gnome/desktop/input-sources/xkb-options";
-    let target_settings = "['caps:ctrl_modifier', 'compose:ralt', 'lv3:menu_switch']";
+    let default_settings = "['caps:ctrl_modifier', 'compose:ralt', 'lv3:menu_switch']";
+    let model_m_settings =
+        "['compose:ralt', 'lv3:menu_switch', 'caps:ctrl_modifier', 'altwin:swap_alt_win']";
     let kb_fingerprint = (5050, 0024);
-
-    dconf::set(xkb_opt, target_settings).unwrap();
 
     loop {
         for device in rusb::devices().unwrap().iter() {
             let device_desc = device.device_descriptor().unwrap();
             if (device_desc.vendor_id(), device_desc.product_id()) == kb_fingerprint {
-                println!("Model M detected")
+                if dconf::get(xkb_opt).unwrap() == model_m_settings {
+                    break;
+                };
+                dconf::set(xkb_opt, model_m_settings).unwrap();
+                break;
             }
+            if dconf::get(xkb_opt).unwrap() == default_settings {
+                break;
+            };
+            dconf::set(xkb_opt, default_settings).unwrap();
+            break;
         }
         sleep(Duration::from_secs(5))
     }
