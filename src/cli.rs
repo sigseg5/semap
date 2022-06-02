172672d67b0de6a9052c40569bb29416082fea7f
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-06-02 10:59:29 +0300

    Add platforms check

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 pub fn get_devices() -> Vec<(u16, u16)> {
     let mut devices = Vec::new();
     for device in rusb::devices().unwrap().iter() {
@@ -5,4 +7,14 @@ pub fn get_devices() -> Vec<(u16, u16)> {
         devices.push((dev.vendor_id(), dev.product_id()));
     }
     devices
+}
+
+pub fn check_platform() {
+    if cfg!(windows) {
+        println!("Windows is not supported right now. And it won't be.");
+        exit(1);
+    } else if cfg!(macos) {
+        println!("macOS is not yet supported.");
+        exit(1);
+    }
 }
