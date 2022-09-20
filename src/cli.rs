1375ddf5105ad28ff1fd3d5e01621cd31ad9e81d
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-09-20 23:13:33 +0300

    Init parse subsystem refactoring

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,7 @@
 use std::process::exit;
+use std::{thread, time};
 
+// TODO: Make private
 pub fn get_devices() -> Vec<(u16, u16)> {
     let mut devices = Vec::with_capacity(2);
     rusb::devices().unwrap().iter().for_each(|device| {
@@ -12,11 +14,33 @@ pub fn get_devices() -> Vec<(u16, u16)> {
 }
 
 pub fn check_platform() {
-    if cfg!(windows) {
-        println!("Windows is not supported right now. And it won't be.");
-        exit(1);
-    } else if cfg!(target_os = "macos") {
-        println!("macOS is not yet supported.");
+    // TODO: unix -> linux
+    if !cfg!(unix) {
+        println!("Only GNU/Linux systems are supported now.");
         exit(1);
     }
+}
+
+// TODO: Add doc
+pub fn find_device() {
+    println!("Disconnect your Model M now (you have 5 sec)…");
+    let five_secs = time::Duration::from_secs(5);
+    thread::sleep(five_secs);
+    let pre_connect_devices = get_devices();
+    println!("Now connect your Model M (you have 5 sec)…");
+    thread::sleep(five_secs);
+    let after_connect_devices = get_devices();
+    let mut difference = vec![];
+    after_connect_devices.iter().for_each(|i| {
+        if !pre_connect_devices.contains(i) {
+            difference.push(&*i);
+        }
+    });
+    println!(
+        "You fingerprint is {:#X?}\n
+                Put this data to 50 line in main.rs, after that rebuild and reinstall service.\n
+                Data format: (0xYYYY, 0xYYYY), replace all y's with fingerprint.",
+        difference
+    );
+    exit(0);
 }
